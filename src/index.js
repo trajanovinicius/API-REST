f10a2b593bfb815e8cab4bd227a81456501c0f0b
viniluk <viniluk9@gmail.com>
2021-07-19 14:05:22 -0300

    iniciando projeto

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express'); // definindo libs
+const bodyParser = require('body-parser')
+
+const app = express();// criando aplicação
+
+app.use(bodyParser.json());// indicando o que vai usar e as funções
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.listen(3000)// qual porta quero ouvir 
